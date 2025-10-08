4f1de566f4f878f34447c02fd0292fbb1bedb75a
Caioba <caioba41342@outlook.com>
2025-10-07 21:38:22 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      // encaminha /api/* para o Express na porta 3000
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+  root: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
   }
 });
