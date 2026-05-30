706656238fbea35bca5466bf77332dc2211155ba
Caioba <caioba41342@outlook.com>
2026-05-30 19:58:34 -0300

    Atualiza portal Vercel de download

---
+++ api/logout.js
@@ -0,0 +1,14 @@
+const { clearSessionCookie } = require("./_auth.js");
+
+module.exports = function handler(req, res) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.statusCode = 405;
+    return res.end(JSON.stringify({ error: "Método não permitido." }));
+  }
+
+  res.setHeader("Set-Cookie", clearSessionCookie());
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.end(JSON.stringify({ ok: true }));
+};
