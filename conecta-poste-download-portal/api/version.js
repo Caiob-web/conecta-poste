70ae56a69af445dd7d7e174cab789e499c430746
Caioba <caioba41342@outlook.com>
2026-06-20 08:41:42 -0300

    Atualiza portal para v1.0.61

--- conecta-poste-download-portal/api/version.js
+++ conecta-poste-download-portal/api/version.js
@@ -1,4 +1,4 @@
-const FALLBACK_VERSION = "1.0.60";
+const FALLBACK_VERSION = "1.0.61";
 const RELEASE_API_URL = "https://api.github.com/repos/Caiob-web/conecta-poste/releases/latest";
 
 function extractVersion(tagName = "") {
